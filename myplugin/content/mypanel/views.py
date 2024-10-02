1a804b9f94e2932d3189918fa8ef1b68035e583f
samuelhilpert <116372172+samuelhilpert@users.noreply.github.com>
2024-10-02 21:15:49 +0200

    first edits

--- myplugin/content/mypanel/views.py
+++ myplugin/content/mypanel/views.py
@@ -3,3 +3,7 @@ from django.views import generic
 
 class IndexView(generic.TemplateView):
     template_name = 'identity/mypanel/index.html'
+
+def index(request):
+    context = {'custom_text': "Dieser Text ist größer und definiert!"}
+    return render(request, 'mypanel/index.html', context)
