fc60cf5ab8cbcd0828e793551f10283f8d8f23c6
samuelhilpert <116372172+samuelhilpert@users.noreply.github.com>
2024-10-02 21:56:56 +0200

    first edits

--- myplugin/content/mypanel/urls.py
+++ myplugin/content/mypanel/urls.py
@@ -4,4 +4,7 @@ from myplugin.content.mypanel import views
 
 urlpatterns = [
     re_path(r'^$', views.IndexView.as_view(), name='index'),
+    re_path(r'^second/$', views.second_page, name='second_page'),
 ]
+
+
--- myplugin/content/mypanel/views.py
+++ myplugin/content/mypanel/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 from django.views import generic
+from django.http import HttpResponse
+from django.template import loader
 
 
 class IndexView(generic.TemplateView):
     template_name = 'identity/mypanel/index.html'
 
-def index(request):
-    context = {'custom_text': "Dieser Text ist größer und definiert!"}
-    return render(request, 'mypanel/index.html', context)
+def testing(request):
+    template = loader.get_template('index.html')
+    context = {
+        'firstname': 'Linus',
+    }
+    return HttpResponse(template.render(context, request))
+
+def second_page(request):
+    return render(request, 'mypanel/second_page.html')
