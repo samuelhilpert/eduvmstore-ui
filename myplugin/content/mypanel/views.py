8701a3fe8639947595523c19d998d8312ce40186
samuelhilpert <116372172+samuelhilpert@users.noreply.github.com>
2024-10-04 15:35:26 +0200

    first edits

--- myplugin/content/mypanel/views.py
+++ myplugin/content/mypanel/views.py
@@ -7,8 +7,18 @@ from django.template import loader
 class IndexView(generic.TemplateView):
     template_name = 'identity/mypanel/index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['panel_title'] = _("My Panel")  # Setze den Titel für das Panel
+        return context
+
 class SecondPageView(generic.TemplateView):
     template_name = 'identity/mypanel/second_page.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['panel_title'] = _("Second Page")  # Setze den Titel für die zweite Seite
+        return context
 
 
 def testing(request):
