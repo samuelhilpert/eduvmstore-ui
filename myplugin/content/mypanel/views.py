54863e0cc6a6ccab75fb4bce904d8db790254238
samuelhilpert <116372172+samuelhilpert@users.noreply.github.com>
2024-10-04 18:38:52 +0200

    first edits

--- myplugin/content/mypanel/views.py
+++ myplugin/content/mypanel/views.py
@@ -7,19 +7,16 @@ from django.utils.translation import gettext_lazy as _
 
 class IndexView(generic.TemplateView):
     template_name = 'identity/mypanel/index.html'
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['panel_title'] = _("My Panel")  # Setze den Titel für das Panel
+        context['panel_title'] = _("My Panel")
+        context['current_user'] = self.request.user  # Aktuell angemeldeten Benutzer hinzufügen
         return context
+
 
 class SecondPageView(generic.TemplateView):
     template_name = 'identity/mypanel/second_page.html'
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['panel_title'] = _("Second Page")  # Setze den Titel für die zweite Seite
-        return context
 
 
 def testing(request):
