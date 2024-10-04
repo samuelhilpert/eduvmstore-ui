b226c38200096255d4e4a8c146bc1f7293702b62
samuelhilpert <116372172+samuelhilpert@users.noreply.github.com>
2024-10-04 22:26:59 +0200

    first edits

--- myplugin/content/eduvmstore/urls.py
+++ myplugin/content/eduvmstore/urls.py
@@ -1,7 +1,7 @@
 from django.urls import re_path
 from django.urls import path
 
-from myplugin.content.mypanel import views
+from myplugin.content.eduvmstore import views
 
 urlpatterns = [
     re_path(r'^$', views.IndexView.as_view(), name='index'),
