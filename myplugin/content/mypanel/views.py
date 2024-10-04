200cd563dab8fa36b354a82a43716af0e18b2f1d
samuelhilpert <116372172+samuelhilpert@users.noreply.github.com>
2024-10-04 14:58:56 +0200

    first edits

--- myplugin/content/mypanel/views.py
+++ myplugin/content/mypanel/views.py
@@ -2,15 +2,13 @@ from django.shortcuts import render
 from django.views import generic
 from django.http import HttpResponse, JsonResponse
 from django.template import loader
-from horizon import views
 
 
 class IndexView(generic.TemplateView):
     template_name = 'identity/mypanel/index.html'
 
-class SecondPageView(views.HorizonTemplateView):
-    template_name = 'mypanel/second_page.html'
-    page_title = _("Second Page")
+class SecondPageView(generic.TemplateView):
+    template_name = 'identity/mypanel/second_page.html'
 
 
 def testing(request):
