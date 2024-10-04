9cbcfbcea9e172bad671b0aee25a46b146fd8bdd
samuelhilpert <116372172+samuelhilpert@users.noreply.github.com>
2024-10-04 14:08:16 +0200

    first edits

--- myplugin/content/mysecondpanel/panel.py
+++ myplugin/content/mysecondpanel/panel.py
@@ -2,6 +2,6 @@ from django.utils.translation import gettext_lazy as _
 import horizon
 
 
-class MyPanel(horizon.Panel):
+class MySecondPanel(horizon.Panel):
     name = _("Admin")
     slug = "mypanel"
