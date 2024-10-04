8bdc748d0f7c17d83cf017a3f4074117e508384a
samuelhilpert <116372172+samuelhilpert@users.noreply.github.com>
2024-10-04 14:50:06 +0200

    first edits

--- myplugin/content/mypanel/urls.py
+++ myplugin/content/mypanel/urls.py
@@ -1,8 +1,10 @@
 from django.urls import re_path
+from django.urls import path
 
 from myplugin.content.mypanel import views
 
 urlpatterns = [
     re_path(r'^$', views.IndexView.as_view(), name='index'),
+    path('second_page/', views.SecondPageView.as_view(), name='second_page'),
 
 ]
--- myplugin/content/mypanel/views.py
+++ myplugin/content/mypanel/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 from django.views import generic
 from django.http import HttpResponse, JsonResponse
 from django.template import loader
+from horizon import views
 
 
 class IndexView(generic.TemplateView):
     template_name = 'identity/mypanel/index.html'
 
-
+class SecondPageView(views.HorizonTemplateView):
+    template_name = 'mypanel/second_page.html'
+    page_title = _("Second Page")
 
 
 def testing(request):
