893baef807068f27d7ad751eb7c06834d61abbfc
samuelhilpert <116372172+samuelhilpert@users.noreply.github.com>
2024-10-06 01:01:30 +0200

    Add My Account First Ideas

--- myplugin/content/eduvmstore/views.py
+++ myplugin/content/eduvmstore/views.py
@@ -9,20 +9,46 @@ from django.conf import settings
 
 
 class IndexView(generic.TemplateView):
-    template_name = 'identity/eduvmstore/index.html'
+    template_name = 'identity/eduvmstore/account.html'
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         user = self.request.user
         token_id = None
 
-
         if hasattr(self.request, "user") and hasattr(self.request.user, "token"):
             token_id = self.request.user.token.id
 
+        # OpenStack API call to get user data
+        keystone_url = f"http://192.168.64.16/identity/v3/users/{user.id}"
+        headers = {
+            "X-Auth-Token": token_id,
+        }
 
-        context['username'] = user.username
-        context['auth_token'] = token_id
-        context['mail'] = user.email
+        try:
+            response = requests.get(keystone_url, headers=headers)
+            response.raise_for_status()
+            user_data = response.json()['user']
+
+            # Add user data to context
+            context['username'] = user_data.get('name')
+            context['mail'] = user_data.get('email')
+        except requests.exceptions.RequestException as e:
+            context['error'] = _("Could not retrieve user information: %s") % str(e)
+
+        # Retrieve projects for the current user
+        projects_url = f"http://192.168.64.16/identity/v3/projects"
+        params = {
+            'user_id': user.id,
+        }
+
+        try:
+            response = requests.get(projects_url, headers=headers, params=params)
+            response.raise_for_status()
+            projects = response.json()['projects']
+            context['projects'] = projects
+        except requests.exceptions.RequestException as e:
+            context['error_projects'] = _("Could not retrieve projects: %s") % str(e)
 
         return context
 
