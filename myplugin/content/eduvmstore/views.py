b3cb8f95f93ca3041b50fda3ba9c0b54c413028a
samuelhilpert <116372172+samuelhilpert@users.noreply.github.com>
2024-10-05 16:55:42 +0200

    Add My Account First Ideas

--- myplugin/content/eduvmstore/views.py
+++ myplugin/content/eduvmstore/views.py
@@ -13,19 +13,42 @@ class IndexView(generic.TemplateView):
         token_id = None
 
 
-        if hasattr(self.request, "user") and hasattr(self.request.user, "token"):
-            token_id = self.request.user.token.id
+        if user.is_authenticated:
+            if hasattr(user, "token"):
+                token_id = user.token.id
 
-
-        context['username'] = user.username
-        context['auth_token'] = token_id
-        context['mail'] = user.email
+            context['username'] = user.username
+            context['auth_token'] = token_id
+            context['mail'] = user.email if hasattr(user, 'email') else 'No email available'
+        else:
+            context['username'] = 'Guest'
+            context['auth_token'] = None
+            context['mail'] = 'Not available'
 
         return context
 
 
 class AccountPageView(generic.TemplateView):
     template_name = 'identity/eduvmstore/account.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+        token_id = None
+
+
+        if user.is_authenticated:
+            if hasattr(user, "token"):
+                token_id = user.token.id
+
+            context['username'] = user.username
+            context['auth_token'] = token_id
+            context['mail'] = user.email if hasattr(user, 'email') else 'No email available'
+        else:
+             context['username'] = 'Guest'
+             context['auth_token'] = None
+             context['mail'] = 'Not available'
+
+        return context
 
 
 
