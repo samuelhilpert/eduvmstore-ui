60c4cdb27d8b258fab9fc04afa7158ee8d9835a1
samuelhilpert <116372172+samuelhilpert@users.noreply.github.com>
2024-10-06 00:49:09 +0200

    Add My Account First Ideas

--- myplugin/content/eduvmstore/views.py
+++ myplugin/content/eduvmstore/views.py
@@ -1,8 +1,11 @@
+import requests
 from django.shortcuts import render
 from django.views import generic
 from django.http import HttpResponse, JsonResponse
 from django.template import loader
 from django.utils.translation import gettext_lazy as _
+from django.conf import settings
+
 
 
 class IndexView(generic.TemplateView):
@@ -26,6 +29,7 @@ class IndexView(generic.TemplateView):
 
 class AccountPageView(generic.TemplateView):
     template_name = 'identity/eduvmstore/account.html'
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         user = self.request.user
@@ -34,10 +38,36 @@ class AccountPageView(generic.TemplateView):
         if hasattr(self.request, "user") and hasattr(self.request.user, "token"):
             token_id = self.request.user.token.id
 
+        # OpenStack API call to get user data
+        keystone_url = f"http://192.168.64.16/identity/v3/users/{user.id}"
+        headers = {
+            "X-Auth-Token": token_id,
+        }
 
-        context['username'] = user.username
-        context['auth_token'] = token_id
-        context['mail'] = user.email
+        try:
+            response = requests.get(keystone_url, headers=headers)
+            response.raise_for_status()
+            user_data = response.json()['user']
+
+            # Add user data to context
+            context['username'] = user_data.get('name')
+            context['mail'] = user_data.get('email')
+        except requests.exceptions.RequestException as e:
+            context['error'] = _("Could not retrieve user information: %s") % str(e)
+
+        # Retrieve projects for the current user
+        projects_url = f"http://192.168.64.16/identity/v3/projects"
+        params = {
+            'user_id': user.id,
+        }
+
+        try:
+            response = requests.get(projects_url, headers=headers, params=params)
+            response.raise_for_status()
+            projects = response.json()['projects']
+            context['projects'] = projects
+        except requests.exceptions.RequestException as e:
+            context['error_projects'] = _("Could not retrieve projects: %s") % str(e)
 
         return context
 
