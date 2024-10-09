ca64fc98b901bdf5085cc50ea37a84018f66e667
emese <emese.katona@outlook.de>
2024-10-09 17:01:29 +0200

    image table added

--- myplugin/content/eduvmstore/tables.py
+++ myplugin/content/eduvmstore/tables.py
@@ -2,22 +2,37 @@ from django.utils.translation import gettext_lazy as _
 
 from horizon import tables
 
-
+# Custom filter for instances
 class MyFilterAction(tables.FilterAction):
     name = "myfilter"
 
 
 class InstancesTable(tables.DataTable):
-    name = tables.Column('name', \
+    # Define table columns for instances
+    name = tables.Column('name',
                          verbose_name=_("Instance Name"))
-    status = tables.Column('status', \
+    status = tables.Column('status',
                            verbose_name=_("Status"))
-    zone = tables.Column('availability_zone', \
+    zone = tables.Column('availability_zone',
                          verbose_name=_("Availability Zone"))
-    image_name = tables.Column('image_name', \
+    image_name = tables.Column('image_name',
                                verbose_name=_("Image Name"))
 
-    class Meta(object):
+    class Meta:
         name = "instances"
         verbose_name = _("Instances")
+
+
+# Image Table definition, same as you had but with small improvement on translations
+class ImageTable(tables.DataTable):
+    name = tables.Column("name", verbose_name=_("Image Name"))
+    description = tables.Column("description", verbose_name=_("Description"))
+    creator = tables.Column("creator", verbose_name=_("Creator"))
+    min_size = tables.Column("min_size", verbose_name=_("Min. Size (GB)"))
+    version = tables.Column("version", verbose_name=_("Version"))
+
+    class Meta:
+        name = "images"
+        verbose_name = _("Images")
         table_actions = (MyFilterAction,)
+        multi_select = False  # Disable multi-select if not needed
--- myplugin/content/eduvmstore/tabs.py
+++ myplugin/content/eduvmstore/tabs.py
@@ -34,7 +34,33 @@ class InstanceTab(tabs.TableTab):
 
             return []
 
+# New Tab for displaying images
+class ImageTab(tabs.TableTab):
+    name = _("Images Tab")
+    slug = "images_tab"
+    table_classes = (tables.ImageTable,)
+    template_name = ("horizon/common/_detail_table.html")
+    preload = False
+
+    def has_more_data(self, table):
+        return self._has_more
+
+    def get_images_data(self):
+        try:
+            marker = self.request.GET.get(tables.ImageTable._meta.pagination_param, None)
+            images, self._has_more = api.glance.image_list_detailed(
+                self.request,
+                marker=marker,
+                paginate=True)
+            return images
+        except Exception:
+            self._has_more = False
+            error_message = _('Unable to get images')
+            exceptions.handle(self.request, error_message)
+            return []
+
+# Tab group that includes both Instances and Images
 class MypanelTabs(tabs.TabGroup):
     slug = "mypanel_tabs"
-    tabs = (InstanceTab,)
+    tabs = (InstanceTab, ImageTab)  # Added the new ImageTab here
     sticky = True
