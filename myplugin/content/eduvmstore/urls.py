64ad2f1b8f29e0c910ca3aed0f84ca924fdf6022
m0nika6 <monika.bircic@gmail.com>
2024-10-13 12:35:22 +0000

    nav

--- myplugin/content/eduvmstore/urls.py
+++ myplugin/content/eduvmstore/urls.py
@@ -5,5 +5,6 @@ from myplugin.content.eduvmstore import views
 
 urlpatterns = [
     re_path(r'^$', views.IndexView.as_view(), name='index'),
-    path(r'details/$', views.DetailsPageView.as_view(), name='details'),
+    path('details/<str:image_id>/', views.DetailsPageView.as_view(), name='details'),
+
 ]
