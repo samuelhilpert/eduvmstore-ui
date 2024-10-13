4f8a521606cf867101a2a50aa1348e2305b880f8
m0nika6 <monika.bircic@gmail.com>
2024-10-13 12:59:04 +0000

    revert

--- myplugin/content/eduvmstore/urls.py
+++ myplugin/content/eduvmstore/urls.py
@@ -5,6 +5,6 @@ from myplugin.content.eduvmstore import views
 
 urlpatterns = [
     re_path(r'^$', views.IndexView.as_view(), name='index'),
-    path('details/<str:image_id>/', views.DetailsPageView.as_view(), name='details'),
+    path('details/', views.DetailsPageView.as_view(), name='details'),
 
 ]
