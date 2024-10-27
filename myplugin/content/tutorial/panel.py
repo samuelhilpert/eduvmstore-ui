41e84babe4801da80b465a4aaad66346c8b95b4c
m0nika6 <monika.bircic@gmail.com>
2024-10-27 17:18:11 +0000

    comments added to tutorial .py files

--- myplugin/content/tutorial/panel.py
+++ myplugin/content/tutorial/panel.py
@@ -3,5 +3,12 @@ import horizon
 
 
 class TutorialPanel(horizon.Panel):
+    """
+        Custom Horizon panel for the Tutorial section.
+
+        :attributes:
+            - name: The display name for the panel, set to "Tutorial".
+            - slug: A unique identifier for the panel, set to "tutorial".
+    """
     name = _("Tutorial")
     slug = "tutorial"
