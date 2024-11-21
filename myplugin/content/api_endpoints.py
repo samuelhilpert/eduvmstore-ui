1de8514d2d73dd90cdfb1b27a09c0b1371403485
emese <emese.katona@outlook.de>
2024-11-21 13:30:33 +0100

    change base_url

--- myplugin/content/api_endpoints.py
+++ myplugin/content/api_endpoints.py
@@ -1,7 +1,8 @@
 
 # Base URL for the API (if your endpoints share a common base)
 # Change the BASE_URL if needed
-BASE_URL = "http://192.168.64.1:8000/api/"
+BASE_URL = "http://localhost:8000/api/"
+
 
 # Define your API endpoints in a dictionary or as constants
 API_ENDPOINTS = {
