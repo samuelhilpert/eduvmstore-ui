c5acd6ea95bafc4d3e3fc67ee899f4abc0c86479
samuelhilpert <116372172+samuelhilpert@users.noreply.github.com>
2025-03-04 16:09:23 +0100

    Update api_endpoints.py

--- myplugin/content/api_endpoints.py
+++ myplugin/content/api_endpoints.py
@@ -1,6 +1,6 @@
 
 # Base URL for the Backend API
-BASE_URL = "http://141.72.12.222:8000/api/"
+BASE_URL = "http://141.72.12.173:8000/api/"
 
 
 # Define your API endpoints in a dictionary or as constants
