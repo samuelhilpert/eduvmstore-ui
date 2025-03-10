52b90766a9716f75794b43dffdeadc61094b7212
rickertmar <marian.rickert@gmail.com>
2025-03-10 16:46:27 +0100

    cleanup

--- myplugin/content/mypanel/panel.py
+++
@@ -1,7 +0,0 @@
-from django.utils.translation import gettext_lazy as _
-import horizon
-
-
-class MyPanel(horizon.Panel):
-    name = _("My Panel")
-    slug = "mypanel"
--- myplugin/enabled/_31000_my_plugin.py
+++
@@ -1,8 +0,0 @@
-PANEL = 'mypanel'
-PANEL_DASHBOARD = 'identity'
-ADD_PANEL = 'myplugin.content.mypanel.panel.MyPanel'
-ADD_INSTALLED_APPS = ['myplugin']
-ADD_ANGULAR_MODULES = ['horizon.dashboard.identity.myplugin.mypanel']
-AUTO_DISCOVER_STATIC_FILES = True
-ADD_JS_FILES = []
-ADD_HEADER_SECTIONS = ['myplugin.content.mypanel.views.HeaderView',]
