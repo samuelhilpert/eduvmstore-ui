501248165156b9e1fd95a45865f37793dc1bdb80
m0nika6 <monika.bircic@gmail.com>
2025-03-23 18:29:14 +0000

    url fix

--- myplugin/content/eduvmstore/urls.py
+++ myplugin/content/eduvmstore/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('validate-name/', views.validate_name, name='validate_name'),
     path('success/', views.InstanceSuccessView.as_view(), name='success'),
     path('favorite_template/', views.GetFavoriteAppTemplateView.as_view(), name='favorite_template'),
-    path('delete_template/<str:template_id>/', views.DeleteTemplateView, name='delete_template'),
+    path('delete_template/<str:template_id>/', views.DeleteTemplateView.as_view(), name='delete_template'),
     path('delete_favorite_template', views.DeleteFavoriteAppTemplateView.as_view(),
          name='delete_favorite_template'),
 
