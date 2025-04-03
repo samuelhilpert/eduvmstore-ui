eac6c078603a600052bf5a20202575e44369780b
samuelhilpert <116372172+samuelhilpert@users.noreply.github.com>
2025-04-03 14:46:10 +0200

    fix image id

--- myplugin/content/eduvmstore/urls.py
+++ myplugin/content/eduvmstore/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
 
     path('', views.IndexView.as_view(), name='index'),
     path('details/<str:template_id>/', views.DetailsPageView.as_view(), name='details'),
-    path('create/', views.CreateView.as_view(), name='create'),
+    path('create/', views.CreateView.as_view(), name='create_empty'),
+    path('create/<str:template_id>/', views.CreateView.as_view(), name='create_with_template'),
     path('edit/<str:template_id>/', views.EditView.as_view(), name='edit'),
     path('instances/<str:image_id>/', views.InstancesView.as_view(), name='instances'),
     path('validate-name/', views.validate_name, name='validate_name'),
