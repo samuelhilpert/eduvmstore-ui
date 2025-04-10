580757a1409594680bee64cb2a1ada7059160b03
emese <emese.katona@outlook.de>
2025-04-10 22:38:17 +0200

    fix

--- myplugin/content/eduvmstore/views.py
+++ myplugin/content/eduvmstore/views.py
@@ -710,21 +710,12 @@ class EditView(generic.TemplateView):
         context = super().get_context_data(**kwargs)
         app_template = self.get_app_template()
 
-
-        all_security_groups = self.get_security_groups()
-
-        selected_sg_names = {sg.get("name") for sg in app_template.get('security_groups', [])}
-
-        # markiere sie im Kontext
-        for sg in all_security_groups:
-            sg.selected = sg.name in selected_sg_names
-
         image_data = self.get_image_data(app_template.get('image_id', ''))
         context.update({
             'app_template': app_template,
             'image_visibility': image_data.get('visibility', 'N/A'),
             'image_owner': image_data.get('owner', 'N/A'),
-            'security_groups': all_security_groups,
+            'security_groups': self.get_security_groups()
         })
 
         return context
