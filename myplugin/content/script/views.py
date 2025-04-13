37805d90745facb2154effbfeb1980e9de744b78
rickertmar <marian.rickert@gmail.com>
2025-04-13 17:28:05 +0200

    fixing the breadcrumbs of the tutorial pages

--- myplugin/content/script/views.py
+++ myplugin/content/script/views.py
@@ -17,3 +17,7 @@ class IndexView(generic.TemplateView):
     template_name = 'eduvmstore_dashboard/script/index.html'
     page_title = _("Script")
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = self.page_title
+        return context
--- myplugin/content/tutorial/views.py
+++ myplugin/content/tutorial/views.py
@@ -17,3 +17,7 @@ class IndexView(generic.TemplateView):
     template_name = 'eduvmstore_dashboard/tutorial/index.html'
     page_title = _("About EduVMStore")
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = self.page_title
+        return context
