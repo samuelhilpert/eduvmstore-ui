0ed2f9c06e7abad21b4a46ee86388c0581a7debe
samuelhilpert <116372172+samuelhilpert@users.noreply.github.com>
2025-04-18 21:12:51 +0200

    change

--- myplugin/static/js/cloudinit_snippets.js
+++ myplugin/static/js/cloudinit_snippets.js
@@ -1,50 +1,38 @@
 // cloudinit_snippets.js
 const scriptSnippets = {
-    ssh_script: {
-        runcmd: [
-            `# Create directory for private keys
-mkdir -p /home/ubuntu/user_keys
-chmod 700 /home/ubuntu/user_keys
-chown ubuntu:ubuntu /home/ubuntu/user_keys
+    "ssh_script":"runcmd:\r\n  - |\r\n    # Create directory for private keys\r\n    " +
+        "mkdir -p /home/ubuntu/user_keys\r\n    chmod 700 /home/ubuntu/user_keys\r\n    " +
+        "chown ubuntu:ubuntu /home/ubuntu/user_keys\r\n\r\n    # Set first user account as admin\r\n    " +
+        "FIRST_USER=$(head -n 1 /etc/users.txt | cut -d':' -f1)\r\n\r\n    # Create users\r\n    " +
+        "while IFS=: read -r username password; do\r\n      if ! id \"$username\" &>/dev/null; then\r\n " +
+        "       useradd -m -s /bin/bash \"$username\"\r\n        echo \"$username:$password\" | " +
+        "chpasswd\r\n      fi\r\n\r\n      # Create SSH directory and key\r\n      " +
+        "sudo -u \"$username\" mkdir -p /home/\"$username\"/.ssh\r\n      " +
+        "chmod 700 /home/\"$username\"/.ssh\r\n\r\n      # Generate SSH key\r\n      " +
+        "sudo -u \"$username\" ssh-keygen -t rsa -b 2048 -f " +
+        "/home/\"$username\"/.ssh/id_rsa -N \"\"\r\n\r\n      # Set Public Key as authorized_key\r\n    " +
+        "  cat /home/\"$username\"/.ssh/id_rsa.pub >> /home/\"$username\"/.ssh/authorized_keys\r\n    " +
+        "  chmod 600 /home/\"$username\"/.ssh/authorized_keys\r\n      " +
+        "chown -R \"$username:$username\" /home/\"$username\"/.ssh\r\n\r\n     " +
+        " # Secure private keys for the admin & Ubuntu user\r\n      " +
+        "cp /home/\"$username\"/.ssh/id_rsa /home/ubuntu/user_keys/\"$username\"_id_rsa\r\n     " +
+        " chmod 600 /home/ubuntu/user_keys/\"$username\"_id_rsa\r\n      " +
+        "chown ubuntu:ubuntu /home/ubuntu/user_keys/\"$username\"_id_rsa\r\n    " +
+        "done < /etc/users.txt\r\n\r\n    # Set first user as admin\r\n    " +
+        "if [ -n \"$FIRST_USER\" ]; then\r\n      echo \"$FIRST_USER ALL=(ALL) NOPASSWD:ALL\" > " +
+        "/etc/sudoers.d/$FIRST_USER\r\n      chmod 440 /etc/sudoers.d/$FIRST_USER\r\n    fi\r\n\r\n  " +
+        "  # SSH configuration: Disable password login\r\n    " +
+        "sed -i 's/^#\\?PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config\r\n    " +
+        "sed -i 's/^#\\?PermitRootLogin.*/PermitRootLogin prohibit-password/' /etc/ssh/sshd_config\r\n   " +
+        " systemctl restart ssh",
 
-while IFS=: read -r username password; do
-  if ! id "$username" &>/dev/null; then
-    useradd -m -s /bin/bash "$username"
-    echo "$username:$password" | chpasswd
-  fi
-  sudo -u "$username" mkdir -p /home/"$username"/.ssh
-  chmod 700 /home/"$username"/.ssh
-  sudo -u "$username" ssh-keygen -t rsa -b 2048 -f /home/"$username"/.ssh/id_rsa -N ""
-  cat /home/"$username"/.ssh/id_rsa.pub >> /home/"$username"/.ssh/authorized_keys
-  chmod 600 /home/"$username"/.ssh/authorized_keys
-  chown -R "$username:$username" /home/"$username"/.ssh
-  cp /home/"$username"/.ssh/id_rsa /home/ubuntu/user_keys/"$username"_id_rsa
-  chmod 600 /home/ubuntu/user_keys/"$username"_id_rsa
-  chown ubuntu:ubuntu /home/ubuntu/user_keys/"$username"_id_rsa
-done < /etc/users.txt
+    "base_configuration":'package_update: true\\r\\npackage_upgrade: ' +
+        'true\\r\\n\\r\\npackages:\\r\\n  - \\r\\n\\r\\nruncmd:\\r\\n  ' +
+        '-\\r\\n\\r\\nfinal_message: \\"\\"',
 
-# SSH configuration: Disable password login
-sed -i 's/^#\\?PasswordAuthentication.*/PasswordAuthentication no/' /etc/ssh/sshd_config
-sed -i 's/^#\\?PermitRootLogin.*/PermitRootLogin prohibit-password/' /etc/ssh/sshd_config
-systemctl restart ssh`
-        ]
-    },
+    "user_add":'runcmd:\\r\\n  - |\\r\\n    while IFS=\':\' read -r username password; do\\r\\n    ' +
+        '  if ! id \\"$username\\" &>/dev/null; then\\r\\n      ' +
+        '  useradd -m -s \\"/bin/bash\\" \\"$username\\"\\r\\n      ' +
+        '  echo \\"$username:$password\\" | chpasswd\\r\\n      fi\\r\\n    done < /etc/users.txt'
 
-    basic_upgrade: {
-        package_update: true,
-        package_upgrade: true,
-        packages: [],
-        runcmd: []
-    },
-
-    user_creation: {
-        runcmd: [
-            `while IFS=':' read -r username password; do
- if ! id "$username" &>/dev/null; then
-  useradd -m -s "/bin/bash" "$username"
-  echo "$username:$password" | chpasswd
- fi
-done < /etc/users.txt`
-        ]
-    }
 };
