72855fac8d4c7b08c846f21431d008eca839d34c
samuelhilpert <116372172+samuelhilpert@users.noreply.github.com>
2025-04-18 14:41:03 +0200

    second try example in create but the first works already

--- myplugin/content/eduvmstore/views.py
+++ myplugin/content/eduvmstore/views.py
@@ -538,12 +538,37 @@ class AppTemplateView(generic.TemplateView):
                 "per_user_ram_gb": "0.5",
                 "per_user_disk_gb": "5",
                 "per_user_cores": "1",
-                "public": True,
+                "public": False,
                 "instantiation_attributes": [{"name": "version"}, {"name": "packages"}],
                 "account_attributes": [{"name": "username"}, {"name": "password"}],
-                "script": "#cloud-config\nruncmd:\n  - echo Hello Ubuntu"
+                "script": "runcmd:\r\n  - |\r\n    # Create directory for private keys\r\n    "
+                          "mkdir -p /home/ubuntu/user_keys\r\n    chmod 700 /home/ubuntu/user_keys\r\n  "
+                          "  chown ubuntu:ubuntu /home/ubuntu/user_keys\r\n\r\n  "
+                          "  while IFS=: read -r username password; do\r\n   "
+                          "   # Create users\r\n      if ! id \"$username\" &>/dev/null; then\r\n   "
+                          "     useradd -m -s /bin/bash \"$username\"\r\n    "
+                          "    echo \"$username:$password\" | chpasswd\r\n      fi\r\n\r\n  "
+                          "    # Create SSH directory and key\r\n    "
+                          "  sudo -u \"$username\" mkdir -p /home/\"$username\"/.ssh\r\n    "
+                          "  chmod 700 /home/\"$username\"/.ssh\r\n\r\n      # Generate SSH key\r\n    "
+                          "  sudo -u \"$username\" ssh-keygen -t rsa -b 2048 -f "
+                          "/home/\"$username\"/.ssh/id_rsa -N \"\"\r\n\r\n   "
+                          "   # Set Public Key as authorized_key\r\n    "
+                          "  cat /home/\"$username\"/.ssh/id_rsa.pub >> "
+                          "/home/\"$username\"/.ssh/authorized_keys\r\n  "
+                          "    chmod 600 /home/\"$username\"/.ssh/authorized_keys\r\n    "
+                          "  chown -R \"$username:$username\" /home/\"$username\"/.ssh\r\n\r\n    "
+                          "  # Secure private keys for the admin & Ubuntu user\r\n   "
+                          "   cp /home/\"$username\"/.ssh/id_rsa /home/ubuntu/user_keys/\"$username\"_id_rsa\r\n   "
+                          "   chmod 600 /home/ubuntu/user_keys/\"$username\"_id_rsa\r\n  "
+                          "    chown ubuntu:ubuntu /home/ubuntu/user_keys/\"$username\"_id_rsa\r\n  "
+                          "  done < /etc/users.txt\r\n\r\n    # SSH configuration: Disable password login\r\n "
+                          "   sed -i 's/^#\\?PasswordAuthentication.*/PasswordAuthentication no/'"
+                          " /etc/ssh/sshd_config\r\n   "
+                          " sed -i 's/^#\\?PermitRootLogin.*/PermitRootLogin prohibit-password/'"
+                          " /etc/ssh/sshd_config\r\n    systemctl restart ssh"
             },
-            # du kannst weitere Presets wie "gitlab", "jupyter" etc. hier hinzufügen
+
         }
 
         template_id = self.kwargs.get('template_id')
