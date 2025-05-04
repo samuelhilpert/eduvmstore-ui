b51d587eb5c36343b01a882eea668fb5cfea7ee3
samuelhilpert <116372172+samuelhilpert@users.noreply.github.com>
2025-05-04 22:33:28 +0200

    delete more not needed code

--- myplugin/static/js/index.js
+++ myplugin/static/js/index.js
@@ -1,16 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let items = document.querySelectorAll(".template-row");
-    let itemsPerPage = 10;
-    let currentItems = 0;
-
-    function showNextItems() {
-        for (let i = currentItems; i < currentItems + itemsPerPage && i < items.length; i++) {
-            items[i].style.display = "table-row";
-        }
-        currentItems += itemsPerPage;
-    }
-
-    showNextItems();
 
     document.querySelectorAll(".favorite-form").forEach(form => {
         form.addEventListener("submit", function () {
