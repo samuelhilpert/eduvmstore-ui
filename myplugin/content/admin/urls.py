f76cf841c06d37d8ab9d3538cc76bb753b865422
samuelhilpert <116372172+samuelhilpert@users.noreply.github.com>
2025-05-04 19:17:15 +0200

    admin in extra files

--- myplugin/content/admin/urls.py
+++ myplugin/content/admin/urls.py
@@ -2,10 +2,11 @@ from django.urls import re_path
 from django.urls import path
 
 from myplugin.content.admin import views
+from myplugin.content.admin.view import index
 
 
 urlpatterns = [
-    re_path(r'^$', views.IndexView.as_view(), name='index'),
+    re_path(r'^$', index.IndexView.as_view(), name='index'),
     path('update_roles/', views.UpdateRolesView.as_view(), name='update_roles'),
     path('approve_template/', views.ApproveTemplateView.as_view(), name='approve_template'),
     path('delete_template/', views.DeleteTemplateView.as_view(), name='delete_template'),
