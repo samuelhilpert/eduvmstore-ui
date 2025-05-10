012b53ebddd1ddbdce6f1936ca6d2965498eefde
Valentin <v.spam@cze-kalla.de>
2025-05-10 22:28:25 +0200

    change base url back to prod

--- myplugin/content/api_endpoints.py
+++ myplugin/content/api_endpoints.py
@@ -3,7 +3,7 @@
 
 # Samuel Dev
 
-BASE_URL = "http://141.72.12.173:8000/api/"
+# BASE_URL = "http://141.72.12.173:8000/api/"
 # BASE_URL = "http://localhost:8000/api/"
 
 
@@ -11,7 +11,7 @@ BASE_URL = "http://141.72.12.173:8000/api/"
 # BASE_URL = "http://141.72.13.194:8000/api/"
 
 # Production
-# BASE_URL = "http://141.72.12.209:8000/api/"
+BASE_URL = "http://141.72.12.209:8000/api/"
 
 # Define your API endpoints in a dictionary or as constants
 API_ENDPOINTS = {
