9c9603baab9c39a0425c9d135b945306ebd427ef
Valentin <v.spam@cze-kalla.de>
2025-05-10 21:59:06 +0200

    added leading spaces for comment

--- myplugin/content/eduvmstore/presets.py
+++ myplugin/content/eduvmstore/presets.py
@@ -206,7 +206,7 @@ preset_examples = {
         "public": False,
         "instantiation_attributes": [],
         "account_attributes": [{"name": "username"}, {"name": "db_name"}, {"name": "password"}],
-        "script":   "# Create a temporary config file\n"
+        "script":   "  # Create a temporary config file\n"
                     "  - path: /tmp/pg_hba.conf\n"
                     "    permissions: '0644'\n"
                     "    content: |\n"
