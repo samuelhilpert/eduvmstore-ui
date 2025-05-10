f888935bc7434a0121427b92fefb6c3f11fd0639
Valentin <v.spam@cze-kalla.de>
2025-05-10 04:59:45 +0200

    provided the examples with copy paste

--- myplugin/content/example/views.py
+++ myplugin/content/example/views.py
@@ -13,30 +13,87 @@ class IndexView(generic.TemplateView):
         context = super().get_context_data(**kwargs)
         context['page_title'] = self.page_title
 
-        # Add object to the context
-        context["preset_examples"] = preset_examples
-
-        # Add simple preset for the test.html fragment
-        context["object"] = {"name": "Bob"}
-
-        context["example_info"] = {
-            "preset": preset_examples["ubuntu_linux"],
-            "use_case_title": "Ubuntu Linux",
-            "use_case_problem_question" : "How to set up simple Ubuntu Linux instances with SSH setup for users?",
-            "use_case_description" : "This example demonstrates how lecturers can set up simple Ubuntu Linux instances with SSH setup for multiple users. Each user will have their own SSH key generated and securely stored.",
-            "overview" : "Lecturers will create an AppTemplate that includes a cloud-init script to configure the Ubuntu Linux environment and automatically set up SSH access for users. This ensures a streamlined process for deploying and managing the instances.",
-            "learning_objectives" : [
-            "Create an AppTemplate tailored for the course requirements.",
-            "Enable the \"Generate SSH setup for users\" option to automate the SSH setup.",
-            "Launch and manage multiple instances of the Ubuntu Linux environment for their students."],
-            "product_name" : "Ubuntu Linux",
-            "product_url" : "https://ubuntu.com/",
-            "image_name" : "Ubuntu 22.04",
-            "security_group_instructions" : "For this example, select a security group that allows SSH access (port 22).",
-            "instance_count": "1",
-            "flavour" : "mb1.small",
-            "instantiation_attribute_recommendation_values" : [],
-            "link_id" : "ubuntu_linux",
-        }
+        context["example_infos"] = [
+            {
+                "preset": preset_examples["ubuntu_linux"],
+                "use_case_title": "Ubuntu Linux",
+                "use_case_problem_question": "How to set up simple Ubuntu Linux instances with SSH setup for users?",
+                "use_case_description": "This example demonstrates how lecturers can set up simple Ubuntu Linux instances with SSH setup for multiple users. Each user will have their own SSH key generated and securely stored.",
+                "overview": "Lecturers will create an AppTemplate that includes a cloud-init script to configure the Ubuntu Linux environment and automatically set up SSH access for users. This ensures a streamlined process for deploying and managing the instances.",
+                "learning_objectives": [
+                    "Create an AppTemplate tailored for the course requirements.",
+                    'Enable the "Generate SSH setup for users" option to automate the SSH setup.',
+                    "Launch and manage multiple instances of the Ubuntu Linux environment for their students.",
+                ],
+                "product_name": "Ubuntu Linux",
+                "product_url": "https://ubuntu.com/",
+                "image_name": "Ubuntu 22.04",
+                "security_group_instructions": "For this example, select a security group that allows SSH access (port 22).",
+                "instance_count": "1",
+                "flavour": "mb1.small",
+                "instantiation_attribute_recommendation_values": [],
+                "link_id": "ubuntu_linux",
+            },
+            {
+                "preset": preset_examples["gitlab_server"],
+                "use_case_title": "Ubuntu Linux",
+                "use_case_problem_question": "How to set up simple Ubuntu Linux instances with SSH setup for users?",
+                "use_case_description": "This example demonstrates how lecturers can set up simple Ubuntu Linux instances with SSH setup for multiple users. Each user will have their own SSH key generated and securely stored.",
+                "overview": "Lecturers will create an AppTemplate that includes a cloud-init script to configure the Ubuntu Linux environment and automatically set up SSH access for users. This ensures a streamlined process for deploying and managing the instances.",
+                "learning_objectives": [
+                    "Create an AppTemplate tailored for the course requirements.",
+                    'Enable the "Generate SSH setup for users" option to automate the SSH setup.',
+                    "Launch and manage multiple instances of the Ubuntu Linux environment for their students.",
+                ],
+                "product_name": "Ubuntu Linux",
+                "product_url": "https://ubuntu.com/",
+                "image_name": "Ubuntu 22.04",
+                "security_group_instructions": "For this example, select a security group that allows SSH access (port 22).",
+                "instance_count": "1",
+                "flavour": "mb1.small",
+                "instantiation_attribute_recommendation_values": [],
+                "link_id": "ubuntu_linux",
+            },
+            {
+                "preset": preset_examples["jupyter_notebook"],
+                "use_case_title": "Ubuntu Linux",
+                "use_case_problem_question": "How to set up simple Ubuntu Linux instances with SSH setup for users?",
+                "use_case_description": "This example demonstrates how lecturers can set up simple Ubuntu Linux instances with SSH setup for multiple users. Each user will have their own SSH key generated and securely stored.",
+                "overview": "Lecturers will create an AppTemplate that includes a cloud-init script to configure the Ubuntu Linux environment and automatically set up SSH access for users. This ensures a streamlined process for deploying and managing the instances.",
+                "learning_objectives": [
+                    "Create an AppTemplate tailored for the course requirements.",
+                    'Enable the "Generate SSH setup for users" option to automate the SSH setup.',
+                    "Launch and manage multiple instances of the Ubuntu Linux environment for their students.",
+                ],
+                "product_name": "Ubuntu Linux",
+                "product_url": "https://ubuntu.com/",
+                "image_name": "Ubuntu 22.04",
+                "security_group_instructions": "For this example, select a security group that allows SSH access (port 22).",
+                "instance_count": "1",
+                "flavour": "mb1.small",
+                "instantiation_attribute_recommendation_values": [],
+                "link_id": "ubuntu_linux",
+            },
+            {
+                "preset": preset_examples["postgres"],
+                "use_case_title": "Ubuntu Linux",
+                "use_case_problem_question": "How to set up simple Ubuntu Linux instances with SSH setup for users?",
+                "use_case_description": "This example demonstrates how lecturers can set up simple Ubuntu Linux instances with SSH setup for multiple users. Each user will have their own SSH key generated and securely stored.",
+                "overview": "Lecturers will create an AppTemplate that includes a cloud-init script to configure the Ubuntu Linux environment and automatically set up SSH access for users. This ensures a streamlined process for deploying and managing the instances.",
+                "learning_objectives": [
+                    "Create an AppTemplate tailored for the course requirements.",
+                    'Enable the "Generate SSH setup for users" option to automate the SSH setup.',
+                    "Launch and manage multiple instances of the Ubuntu Linux environment for their students.",
+                ],
+                "product_name": "Ubuntu Linux",
+                "product_url": "https://ubuntu.com/",
+                "image_name": "Ubuntu 22.04",
+                "security_group_instructions": "For this example, select a security group that allows SSH access (port 22).",
+                "instance_count": "1",
+                "flavour": "mb1.small",
+                "instantiation_attribute_recommendation_values": [],
+                "link_id": "ubuntu_linux",
+            },
+        ]
 
         return context
