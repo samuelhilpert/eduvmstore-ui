34a5b8368fd8602f39dc37490906475d5b2070bf
Valentin <v.spam@cze-kalla.de>
2025-05-10 04:21:31 +0200

    Try to have the ubuntu example in views

--- myplugin/content/example/views.py
+++ myplugin/content/example/views.py
@@ -19,4 +19,24 @@ class IndexView(generic.TemplateView):
         # Add simple preset for the test.html fragment
         context["object"] = {"name": "Bob"}
 
+        context["example_info"] = {
+            "preset": preset_examples.ubuntu_linux,
+            "use_case_title": "Ubuntu Linux",
+            "use_case_problem_question" : "How to set up simple Ubuntu Linux instances with SSH setup for users?",
+            "use_case_description" : "This example demonstrates how lecturers can set up simple Ubuntu Linux instances with SSH setup for multiple users. Each user will have their own SSH key generated and securely stored.",
+            "overview" : "Lecturers will create an AppTemplate that includes a cloud-init script to configure the Ubuntu Linux environment and automatically set up SSH access for users. This ensures a streamlined process for deploying and managing the instances.",
+            "learning_objectives" : [
+            "Create an AppTemplate tailored for the course requirements.",
+            "Enable the \"Generate SSH setup for users\" option to automate the SSH setup.",
+            "Launch and manage multiple instances of the Ubuntu Linux environment for their students."],
+            "product_name" : "Ubuntu Linux",
+            "product_url" : "https://ubuntu.com/",
+            "image_name" : "Ubuntu 22.04",
+            "security_group_instructions" : "For this example, select a security group that allows SSH access (port 22).",
+            "instance_count": "1",
+            "flavour" : "mb1.small",
+            "instantiation_attribute_recommendation_values" : [],
+            "link_id" : "ubuntu_linux",
+        }
+
         return context
